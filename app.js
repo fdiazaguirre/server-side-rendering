b669cbca4271473c78e92846cabd6b76075420ec
fdiazaguirre <fdiazaguirre@gmail.com>
2016-07-21 16:34:48 -0700

    Replace welcome page with pug template.

--- app.js
+++ app.js
@@ -9,8 +9,11 @@ var express = require('express')
 // Static content.
 app.use(express.static('resources'));
 
+app.set('view engine', 'pug');
+app.set('views', process.cwd());
+
 app.get('/', function (req, res) {
-  res.sendFile(process.cwd() + '/welcome-page.html');
+  res.render('welcome-page', { title: 'Hey', message: 'Welcome here!'});
 });
 
 server.listen(port);
